36522e43934bf43c1775325bb26fdf534dc52577
Cipher <74297154+Cipher58@users.noreply.github.com>
2023-07-28 21:07:55 +0100

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,38 @@
+def create_cipher_key():
+    # Define your custom substitution key here
+    # For simplicity, let's use a predefined key with symbols for each letter
+    key = {
+        'a': '@', 'b': '!', 'c': '#', 'd': '$', 'e': '%', 'f': '&', 'g': '*',
+        'h': '(', 'i': ')', 'j': '+', 'k': '-', 'l': '/', 'm': '=', 'n': '?',
+        'o': '[', 'p': ']', 'q': '{', 'r': '}', 's': '<', 't': '>', 'u': '|',
+        'v': '^', 'w': ':', 'x': ';', 'y': '"', 'z': "'"
+    }
+    return key
+
+def encrypt(message, key):
+    encrypted_message = ""
+    for char in message.lower():
+        if char in key:
+            encrypted_message += key[char]
+        else:
+            encrypted_message += char
+    return encrypted_message
+
+def decrypt(encrypted_message, key):
+    reverse_key = {value: key for key, value in key.items()}
+    decrypted_message = ""
+    for char in encrypted_message:
+        if char in reverse_key:
+            decrypted_message += reverse_key[char]
+        else:
+            decrypted_message += char
+    return decrypted_message
+
+# Example usage:
+cipher_key = create_cipher_key()
+plaintext = input("You: ")
+encrypted_text = encrypt(plaintext, cipher_key)
+print("Encrypted:", encrypted_text)
+
+decrypted_text = decrypt(encrypted_text, cipher_key)
+print("Decrypted:", decrypted_text)
